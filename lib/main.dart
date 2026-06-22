c5fd0ae02e1e74aaf538cc2db7d21f1578560984
Huseyin Akdeniz <GITHUB_MAIL_ADRESIN>
2026-06-22 10:20:35 +0300

    Set login as first screen and allow guest app reads

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'dart:html' as html;
-import 'dart:js_util' as js_util;
-
-import 'package:firebase_core/firebase_core.dart';
+﻿import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
-import 'screens/install_prompt_screen.dart';
 import 'screens/login_screen.dart';
 
 Future<void> main() async {
@@ -21,21 +17,8 @@ Future<void> main() async {
 class AlphaEventsGuestApp extends StatelessWidget {
   const AlphaEventsGuestApp({super.key});
 
-  bool isRunningAsInstalledApp() {
-    final bool displayModeStandalone = html.window
-        .matchMedia('(display-mode: standalone)')
-        .matches;
-
-    final bool iosStandalone =
-        js_util.getProperty(html.window.navigator, 'standalone') == true;
-
-    return displayModeStandalone || iosStandalone;
-  }
-
   @override
   Widget build(BuildContext context) {
-    final bool installedAppMode = isRunningAsInstalledApp();
-
     return MaterialApp(
       title: 'Alpha Events Guest Portal',
       debugShowCheckedModeBanner: false,
@@ -45,9 +28,7 @@ class AlphaEventsGuestApp extends StatelessWidget {
         fontFamily: 'Arial',
         useMaterial3: true,
       ),
-      home: installedAppMode
-          ? const LoginScreen()
-          : const InstallPromptScreen(),
+      home: const LoginScreen(),
     );
   }
 }
